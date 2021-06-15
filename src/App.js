57e747325d29fce19828d729fdd0804c0ea5cb31
wasabi <wasabi.c93@gmail.com>
2021-06-15 15:59:38 +0700

    simple

--- src/App.js
+++ src/App.js
@@ -4,50 +4,61 @@ import './App.css';
 
 export default function App() {
     const [current, setCurrent] = useState(0)
-    const [total, setTotal] = useState(0)
-    const [arr, setArr] = useState([0])
+    const [arr, setArr] = useState([])
+
+    function clear() {
+        setCurrent(0)
+        setArr([])
+    }
 
     function handleClick(e) {
-        e.preventDefault()
-        setCurrent(e.targer.value)
+
+        setArr([...arr,e.target.value])
+        // arr.push(e.target.value)
     }
 
-    function handleDivide() {
-
+    function total() {
+        return setCurrent(eval(current))
     }
+    
+    useEffect(() => {
+        setCurrent(arr.join(''))
+    },[arr])
 
     return (
+        <div>
         <table className='border'>
             <tbody>
             <tr><td colSpan='4' className='td-1'>{current}</td></tr>
             <tr>
-                <td colSpan='3'><button>C</button></td>
-                <td onClick={handleDivide}><button>/</button></td>
+                <td colSpan='3' onClick={clear}><button>C</button></td>
+                <td ><button value='/' onClick={handleClick}>/</button></td>
             </tr>
             <tr>
-                <td><button>7</button></td>
-                <td><button>8</button></td>
-                <td><button>9</button></td>
-                <td><button>x</button></td>
+                <td><button value='7' onClick={handleClick}>7</button></td>
+                <td><button value='8' onClick={handleClick}>8</button></td>
+                <td><button value='9' onClick={handleClick}>9</button></td>
+                <td><button value='*' onClick={handleClick}>x</button></td>
             </tr>
             <tr>
-                <td><button>4</button></td>
-                <td><button>5</button></td>
-                <td><button>6</button></td>
-                <td><button>-</button></td>
+                <td><button value='4' onClick={handleClick}>4</button></td>
+                <td><button value='5' onClick={handleClick}>5</button></td>
+                <td><button value='6' onClick={handleClick}>6</button></td>
+                <td><button value='-' onClick={handleClick}>-</button></td>
             </tr>
             <tr>
-                <td><button>1</button></td>
-                <td><button>2</button></td>
-                <td><button>3</button></td>
-                <td><button>+</button></td>
+                <td><button value='1' onClick={handleClick}>1</button></td>
+                <td><button value='2' onClick={handleClick}>2</button></td>
+                <td><button value='3' onClick={handleClick}>3</button></td>
+                <td><button value='+' onClick={handleClick}>+</button></td>
             </tr>
             <tr>
-                <td colSpan='2'><button>0</button></td>
-                <td><button>,</button></td>
-                <td><button>=</button></td>
+                <td colSpan='2'><button value='0' onClick={handleClick}>0</button></td>
+                <td><button value='.' onClick={handleClick}>.</button></td>
+                <td><button onClick={total}>=</button></td>
             </tr>
             </tbody>
         </table>
+        </div>
     )
 }
